91654be37a633f6667f4da92f3561b184a70a5e9
Jordan Batalla <healtaker@hotmail.com>
2022-06-08 15:46:38 -0300

    creo que es asi xd

---
+++ resta.py
@@ -0,0 +1,10 @@
+print("Funcion de resta con Python")
+def resta():
+    a = int(input('Ingrese el primer numero:'))
+    b = int(input('Ingrese un segundo numero:'))
+    resultado = a -b
+    print ("el resultado de la resta entre:",a, "y",b, "es:" , resultado)
+
+
+resta()
+
